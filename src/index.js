91826369ae9ff3be10788bec0acedd50d0341f3c
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-02-17 14:18:42 +0100

    creating an h1 element in the App

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 function App() {
-  return <header></header>;
+  return <h1>"Hello React!</h1>;
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
