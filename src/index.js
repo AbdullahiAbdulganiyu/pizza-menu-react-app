6c4c7163fcea362d33c76496e8545b1ad79322a8
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-02-17 23:43:59 +0100

    adding the logic to a variable and commenting previos code

--- src/index.js
+++ src/index.js
@@ -76,9 +76,10 @@ function Footer() {
   const hour = new Date().getHours();
   const openHour = 12;
   const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
 
-  if (hour >= openHour && hour <= closeHour) alert("We're currently Open");
-  else alert("Sorry we're closed.");
+  // if (hour >= openHour && hour <= closeHour) alert("We're currently Open");
+  // else alert("Sorry we're closed.");
   return (
     <footer>
       It's {new Date().toLocaleTimeString()}, We're currently Open
