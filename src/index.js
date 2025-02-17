e250055d3aa8d068ad913335a287193e16f7377f
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-02-17 14:22:17 +0100

    rendering App and fixing importation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 function App() {
   return <h1>"Hello React!</h1>;
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<App />);
