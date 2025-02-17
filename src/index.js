9ed61f34d58155dc51e26413d6eb1c76b6f9fe65
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-02-17 15:11:24 +0100

    returning a div from App component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
-  return <h1>"Hello React!</h1>;
+  return (
+    <div>
+      <h1>"Hello React!</h1>
+    </div>
+  );
 }
 
 function Pizza() {
