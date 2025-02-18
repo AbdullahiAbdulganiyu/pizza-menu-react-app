268f0007a64167746582bb4571d7c3c66e9e6a66
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-02-18 17:31:36 +0100

    creating the pizza menu by looping over the pizzaData array, creating a new array{using map) and passing it as the props

--- src/index.js
+++ src/index.js
@@ -71,6 +71,11 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
+      <div>
+        {pizzaData.map((pizza) => (
+          <Pizza pizzaObj={pizza} />
+        ))}
+      </div>
       {/* <Pizza
         name="Pizza Spinaci"
         ingredients="Tomato, mozarella, spinach, and ricotta cheese"
@@ -90,11 +95,11 @@ function Menu() {
 function Pizza(props) {
   return (
     <div className="pizza">
-      <img src={props.photoName} alt={props.name} />
+      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
       <div>
-        <h3>{props.name}</h3>
-        <p>{props.ingredients}</p>
-        <span>{props.price + 3}</span>
+        <h3>{props.pizzaObj.name}</h3>
+        <p>{props.pizzaObj.ingredients}</p>
+        <span>{props.pizzaObj.price + 3}</span>
       </div>
     </div>
   );
