e321bdd48ee0e334b96d88cd9bbe2774a7d7c5db
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-02-18 22:16:28 +0100

    replacing with ternery operator

--- src/index.js
+++ src/index.js
@@ -122,8 +122,12 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="order">
-        {isOpen && (
+        {isOpen ? (
           <p>We're open until {closeHour}:00. Come visit us or order online.</p>
+        ) : (
+          <p>
+            We're Happy to welcome you between {openHour}:00 and {closeHour}:00
+          </p>
         )}
         <button className="btn">Order</button>
       </div>
